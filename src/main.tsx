dfb107d7dc247367ee6388e5d7b01c3183e643d8
YashAgarwal1201 <legoyashx@gmail.com>
2024-10-29 19:38:21 +0530

    fix: navigation fix 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import Router from "./Routes/Routes.tsx";
+import { Suspense } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={Router} />
+  <Suspense
+    fallback={
+      <div className="w-screen h-screen bg-color1 text-color1">Loading...</div>
+    }
+  >
+    <RouterProvider router={Router} />
+  </Suspense>
 );
